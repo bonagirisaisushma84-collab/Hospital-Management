f1421a055ae5ac05ab6e6b25ec003e89237e2936
bonagirisaisushma84-collab <bonagirisaisushma84@gmail.com>
2026-04-21 17:49:29 +0530

    update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,48 +1,48 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
 
-const authRoutes = require("./routes/authRoutes");
-const doctorRoutes = require("./routes/doctorRoutes");
-const patientRoutes = require("./routes/patientRoutes");
-const appointmentRoutes = require("./routes/appointmentRoutes");
-const prescriptionRoutes = require("./routes/prescriptionRoutes");
-const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes")
+const doctorRoutes = require("./routes/doctorRoutes")
+const patientRoutes = require("./routes/patientRoutes")
+const appointmentRoutes = require("./routes/appointmentRoutes")
+const prescriptionRoutes = require("./routes/prescriptionRoutes")
+const userRoutes = require("./routes/userRoutes")
 
-const app = express();
+const app = express()
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Request Logger
 app.use((req, res, next) => {
-    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-    next();
-});
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`)
+    next()
+})
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("connected successfully"))
-    .catch(err => console.log("connection error:", err));
+    .catch(err => console.log("connection error:", err))
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+  res.send("API is running...")
+})
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/doctors", doctorRoutes);
-app.use("/api/patients", patientRoutes);
-app.use("/api/appointments", appointmentRoutes);
-app.use("/api/prescriptions", prescriptionRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes)
+app.use("/api/doctors", doctorRoutes)
+app.use("/api/patients", patientRoutes)
+app.use("/api/appointments", appointmentRoutes)
+app.use("/api/prescriptions", prescriptionRoutes)
+app.use("/api/users", userRoutes)
 
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
